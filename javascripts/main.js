f78b5389714e88984ddba73ae06be642325db8c7
Kevin Davis-Knowlton <brunner314@gmail.com>
2014-11-18 21:39:42 -0500

    Typo fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,6 @@ console.log('This would be the main JS file.');
 
 function calculate() {
   var input = document.getElementById("equation-in")
-  var output = document.getElementByID("equation-out")
+  var output = document.getElementById("equation-out")
   output.innerHTML = input.value
 }
