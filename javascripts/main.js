7df9bd5b7e453660b3b7029938692c95f5adddd5
Kevin Davis-Knowlton <brunner314@gmail.com>
2014-11-18 21:35:41 -0500

    stub out calculate function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+
+function calculate() {
+  var input = document.getElementById("equation-in")
+  var output = document.getElementByID("equation-out")
+  output.innerHTML = input.value
+}
