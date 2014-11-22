cb62d34697f920293ccd0cc21a46d6445dc97a74
Kevin Davis-Knowlton <brunner314@gmail.com>
2014-11-21 22:40:11 -0500

    Rough out calculate and start tokenize

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,5 +3,33 @@ console.log('This would be the main JS file.');
 function calculate() {
   var input = document.getElementById("equation-in")
   var output = document.getElementById("equation-out")
-  output.innerHTML = input.value
+  tokenized = tokenize_expression(input.value)
+  postfixed = shunting_yard(tokenized)
+  result = calculate_rpn(postfixed)
+  output.innerHTML = result
+}
+
+function tokenize_expression(expresion_string) {
+  expression_string = expression_string.replace(/ /g, '')
+  expression_array = new Array()
+  while (expression_string.length > 0) {
+    if (expression_string.search(/d+/) == 0) {
+      expression_array[expression_array.length] = /d+/.exec(expression_string)
+      expression_string = expression_string.replace(/d+/, '')
+    }
+    else if (expression_string.search(/[\+\-\*\/]/) == 0) {
+      expression_array[expression_array.length] = /[\+\-\*\/]/.exec(expression_string)
+      expression_string = expression_string.replace(/[\+\-\*\/]/, '')
+    }
+  }
+  return expression_array
+}
+
+function shunting_yard(infix_expression) {
+  postfix_expression = infix_expression
+  return postfix_expression
+}
+
+function calculate_rpn(expression) {
+  return expression
 }
